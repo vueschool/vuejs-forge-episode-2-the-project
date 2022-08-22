82d9566d04dd33070330c92aa397bb233de74577
Daniel Kelly <me@danielkelly.io>
2022-08-22 14:25:49 -0500

    sort/filters transition

--- pages/index.vue
+++ pages/index.vue
@@ -14,12 +14,32 @@ useAsyncData("products", async () => productStore.fetchProducts());
       v-if="productStore.products"
       class="gap-7 p-10 sm:grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 flex-wrap justify-items-stretch items-stretch"
     >
-      <ProductCard
-        v-for="product in productStore.products"
-        :product="product"
-        :key="product.sys.id"
-        class="mb-5"
-      />
+      <TransitionGroup name="products">
+        <ProductCard
+          v-for="product in productStore.products"
+          :product="product"
+          :key="product.sys.id"
+          class="mb-5"
+        />
+      </TransitionGroup>
     </div>
   </div>
 </template>
+
+<style>
+.product-card {
+  transition: all 0.5s ease-in-out;
+}
+.products-enter-from {
+  transform: scale(0.5) translatey(-80px);
+  opacity: 0;
+}
+.products-leave-to {
+  transform: translatey(30px);
+  opacity: 0;
+}
+.products-leave-active {
+  position: absolute;
+  z-index: -1;
+}
+</style>
